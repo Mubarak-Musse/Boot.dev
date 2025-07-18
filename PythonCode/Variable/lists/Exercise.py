b305c4928842c8f9fb4d4000bc26ff6f17d0b5f8
Mubarak Musse <Mobarak.Musse@gmail.com>
2025-07-18 08:34:10 -0400

    Variable files modified

---
+++ PythonCode/Variable/lists/Exercise.py
@@ -0,0 +1,15 @@
+"""
+Complete the split_players_into_teams function.
+
+It accepts a list of players (strings representing their names) and returns two lists in this order:
+
+1. A new list of all the players with even-numbered indexes in the original list.
+2. A new list of all the players with odd-numbered indexes in the original list.
+
+Use a slice with a "step" to create two new lists from the players list. Don't be afraid to consult your spellbook for list slicing help!
+
+
+"""
+fruits = ["apple", "banana",""]
+def split_player_into_teams(players):
+    players_even_number = []
---
+++ PythonCode/Variable/lists/ReadMe.md
@@ -0,0 +1,11 @@
+# lists
+
+## Defined as a list of comma spearated values btween Square brackets
+
+list maight containe a list of different type , but usually the items have the same type 
+
+- lists can be indexed and sliced 
+- list also support operation like concatenation 
+- list are mutable type , i.e. it is possible to cahnge their  content 
+- you can adjust and add new item at the end of the list using  list.append() method 
+- 
